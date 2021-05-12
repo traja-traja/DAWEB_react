5659c36e4ef423ad7e80c1009541aef3055b4075
Traja <traja@traja.cz>
2021-05-12 15:19:30 +0200

    prepared for user task

--- nakupni-seznam-react/src/components/ShoppingItem.jsx
+++ nakupni-seznam-react/src/components/ShoppingItem.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import "./ShoppingItem.css";
 
 const ShoppingItem = (props) => {
-  const handleClick = () => {
-    console.log("click");
-  };
-
   return (
-    <div className="polozka" onClick={handleClick}>
+    <div className="polozka">
       <div className="polozka__nazev">{props.nazev}</div>
       <div className="polozka__mnozstvi">{props.mnozstvi} ks</div>
     </div>
