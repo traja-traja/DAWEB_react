8033c7f4aaef7200ea3aa1a8bbcae7cb4f053c3f
Traja <traja@traja.cz>
2021-05-12 15:26:15 +0200

    click handler added, toggling

--- nakupni-seznam-react/src/components/ShoppingItem.jsx
+++ nakupni-seznam-react/src/components/ShoppingItem.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShoppingItem.css";
 
 const ShoppingItem = (props) => {
+  const [koupeno, setKoupeno] = useState(false);
+
   const handleClick = () => {
-    console.log("click");
+    setKoupeno(!koupeno);
   };
 
   return (
-    <div className="polozka" onClick={handleClick}>
+    <div
+      className={koupeno ? "polozka polozka--koupeno" : "polozka"}
+      onClick={handleClick}
+    >
       <div className="polozka__nazev">{props.nazev}</div>
       <div className="polozka__mnozstvi">{props.mnozstvi} ks</div>
     </div>
