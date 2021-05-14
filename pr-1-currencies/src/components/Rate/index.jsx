15f4817d6679bc36862e0f51742fdc43ec32f4d5
Traja <traja@traja.cz>
2021-05-14 14:17:49 +0200

    pr-1-currencies-assignment

---
+++ pr-1-currencies/src/components/Rate/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Rate = () => {
+  return (
+    <div className="rate">
+      <div className="rate__currency">1 USD</div>
+      <div>=</div>
+      <div className="rate__value">22 CZK</div>
+    </div>
+  );
+};
+
+export default Rate;
---
+++ pr-1-currencies/src/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import Rate from './components/Rate';
+import './style.css';
+import './index.html';
+
+const App = () => {
+  const [currency, setCurrency] = useState('USD');
+
+  const handleCurrencyChange = (e) => {
+    setCurrency(e.target.value);
+  };
+
+  return (
+    <div className="container">
+      <h1>Kurzovní lístek</h1>
+      <div className="currency-info">
+        <form>
+          <div className="form-field">
+            <label htmlFor="currency-select">Měna:</label>
+            <select
+              id="currency-select"
+              value={currency}
+              onChange={handleCurrencyChange}
+            >
+              <option value="USD">Americký dolar</option>
+              <option value="EUR">Euro</option>
+              <option value="GBP">Britská libra</option>
+            </select>
+          </div>
+        </form>
+        <Rate />
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.querySelector('#app'));
