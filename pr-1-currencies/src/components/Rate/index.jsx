610fdc21f457cfe18f22662dc31b2844e0e19b23
Traja <traja@traja.cz>
2021-05-14 15:15:57 +0200

    pr-1-taks-solved

--- pr-1-currencies/src/components/Rate/index.jsx
+++ pr-1-currencies/src/components/Rate/index.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 const Rate = (props) => {
+  const [rateValue, setRateValue] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `https://api.frankfurter.app/latest?from=${props.currency}&to=CZK`
+    ).then(response => {
+      return response.json();
+    }).then (data => {
+      setRateValue(data.rates.CZK);
+    });
+
+    return () => setRateValue(null);
+  }, [props.currency]);
+
+  if (rateValue === null) {
+    return <div className="rate">Nahrávám data...</div>
+  }
+
   return (
     <div className="rate">
       <div className="rate__currency">1 {props.currency}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      <div className="rate__value">{rateValue} CZK</div>
     </div>
   );
 };
