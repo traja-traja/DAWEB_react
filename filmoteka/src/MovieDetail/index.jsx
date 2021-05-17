cdad3e257173f22c1ddbf69717f864cb45cc2591
traja-traja <traja@traja.cz>
2021-05-18 00:41:36 +0200

    done film :)

--- filmoteka/src/MovieDetail/index.jsx
+++ filmoteka/src/MovieDetail/index.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 export const MovieDetail = () => {
-  const params = useParams();
-
-  console.log(params);
+  const {film} = useParams(); // params.film
 
   return <div>
     <h2>Podrobnosti o filmu</h2>
-    ...
+    <p>{film}</p>
     </div>
 };
